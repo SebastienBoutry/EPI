7b7ef5863b4a9a2162ea50c90354fc55643f6f04
Boutry <sebastien.boutry@inrae.fr>
2023-04-11 17:53:00 +0200

    modification sql

--- SQL/requete_POMET.sql
+++ SQL/requete_POMET.sql
@@ -1,10 +1,11 @@
 -- !preview conn=DBI::dbConnect(RSQLite::SQLite())
 SELECT trait_id, experimentation, fk_materiel_id, madate, station, pos_deb_lat_dd,
 pos_deb_long_dd, pos_fin_lat_dd, pos_fin_long_dd, distance_chalutee, duree,
-nt, pt, espece.nom, nom_fr, engin, masse_eau, salinite_classe, systeme
+nt, pt, espece.nom, espece.nom_fr, engin, masse_eau.masse_eau, salinite_classe, systeme
 FROM trait, echantillon, espece, campagnes, masse_eau
 WHERE trait.trait_id = echantillon.fk_trait_id AND 
 echantillon.espece_id = espece.espece_id AND 
 trait.fk_campagne_id = campagnes.campagne_id AND 
-campagnes.fk_masse_eau = masse_eau.masse_eau_id;
+campagnes.fk_masse_eau = masse_eau.masse_eau_id AND
+espece.subphylum = 'Vertebrata';
                               
